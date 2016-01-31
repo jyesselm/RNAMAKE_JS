c5287e43656d44d80adfa5e5a5e98141d3a870fc
Joseph Yesselman <jyesselm@stanford.edu>
2016-01-30 20:06:08 -0800

    added other directories

---
+++ test/structure/structure.js
@@ -0,0 +1,11 @@
+/**
+ * Created by josephyesselman on 1/30/16.
+ */
+
+module("structure");
+
+test( "constructor", function() {
+    var rts = new RNAMAKE.ResidueTypeSet();
+    //ok( a.min.equals( posInf2 ), "Passed!" );
+    ok(1 == 1, "Passed!");
+});
