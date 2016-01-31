aba8a073a6f182699316c15de93e102ea15aae4a
Joseph Yesselman <jyesselm@stanford.edu>
2016-01-31 13:52:04 -0800

    reworked atom to use THREE.Vector as argument, almost done with residue

--- src/js/structure.js
+++ src/js/structure.js
@@ -6,14 +6,19 @@ RNAMAKE.Atom = function(name, coords) {
     var geometry = new THREE.SphereGeometry(4, 64, 64);
     var material = new THREE.MeshBasicMaterial(  {color: 0xff0000, shading: THREE.SmoothShading} );
 
+    this.name = name;
     this.obj = new THREE.Mesh(geometry, material);
-    this.obj.position.x = coords[0];
-    this.obj.position.y = coords[1];
-    this.obj.position.z = coords[2];
+    this.obj.position.copy(coords);
     this.obj.castShadow = true;
     this.obj.receiveShadow = true;
-    this.name = name;
 
+    this.coords = function() {
+        return this.obj.position;
+    }
+
+    this.copy = function() {
+        return new RNAMAKE.Atom(this.name, this.coords());
+    }
 };
 
 RNAMAKE.ResidueType = function(name, atom_map) {
@@ -56,6 +61,46 @@ RNAMAKE.ResidueTypeSet = function() {
 
 //create a singleton so there is just this one
 RNAMAKE.residue_type_set = new RNAMAKE.ResidueTypeSet();
+
+RNAMAKE.Residue = function(rtype, name, num, chain_id, i_code) {
+    this.rtype    = rtype;
+    this.name     = name;
+    this.num      = num;
+    this.chain_id = chain_id;
+    this.i_code   = i_code;
+    if(this.i_code == undefined) {  this.i_code = ""; }
+    this.atoms = [];
+
+    this.setup_atoms = function(atoms) {
+        for(var i in atoms) {
+            //var name_change
+            if(this.rtype.atom_map.hasOwnProperty(atoms[i].name)) {
+                var pos = this.rtype.atom_map[atoms[i].name];
+            }
+        }
+    };
+};
+
+RNAMAKE.str_to_atom = function(s) {
+    var spl = s.split(" ");
+    var coords = new THREE.Vector3(spl[1], spl[2], spl[3]);
+    return new RNAMAKE.Atom(spl[0], coords);
+}
+
+RNAMAKE.str_to_res = function(s) {
+    var spl = s.split(",");
+    var rtype = RNAMAKE.residue_type_set.get_rtype_by_resname(spl[0]);
+    var r = new RNAMAKE.Residue(rtype, spl[1], spl[2], spl[3], spl[4]);
+    var atoms = [];
+
+    for(var i = 5; i < spl.length-1; i++) {
+        var a = RNAMAKE.str_to_atom(spl[i]);
+        atoms.push(a);
+    }
+    r.setup_atoms(atoms);
+
+    return r;
+};
 
 
 
