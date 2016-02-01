6cdf81c018d1e21665383b0814a0b269bf0ba4ab
Joseph Yesselman <jyesselm@stanford.edu>
2016-02-01 12:06:37 -0800

    changed how drawing is done now is not automatic but done in .draw() like python

--- src/js/structure.js
+++ src/js/structure.js
@@ -2,22 +2,41 @@
  * Created by josephyesselman on 1/30/16.
  */
 
-RNAMAKE.Atom = function(name, coords) {
-    var geometry = new THREE.SphereGeometry(4, 64, 64);
-    var material = new THREE.MeshBasicMaterial(  {color: 0xff0000, shading: THREE.SmoothShading} );
+RNAMAKE.DrawMode = {
+    STICKS: 0
+};
 
-    this.name = name;
-    this.obj = new THREE.Mesh(geometry, material);
-    this.obj.position.copy(coords);
-    this.obj.castShadow = true;
-    this.obj.receiveShadow = true;
-
-    this.coords = function() {
-        return this.obj.position;
+RNAMAKE.Atom = function(name, coords, color) {
+    if(color == undefined) {
+        color = 'red';
     }
+    this.draw_mode = 0;
+    this.drawn = 0;
+    this.coords = coords;
+    this.color = color
+    this.name = name;
+    this.obj = null;
 
     this.copy = function() {
-        return new RNAMAKE.Atom(this.name, this.coords());
+        return new RNAMAKE.Atom(this.name, this.coords);
+    };
+
+    this.draw = function(scene, draw_mode) {
+        if(this.draw_mode == draw_mode && this.drawn) { return; }
+        this.draw_mode = draw_mode;
+        if(this.draw_mode == RNAMAKE.DrawMode.STICKS) {
+            var geometry = new THREE.SphereGeometry(0.25, 64, 64);
+            var material = new THREE.MeshBasicMaterial(  {color: this.color, shading: THREE.SmoothShading} );
+            this.obj = new THREE.Mesh(geometry, material);
+            this.obj.position.copy(this.coords);
+            this.obj.castShadow = true;
+            this.obj.receiveShadow = true;
+            scene.add(this.obj);
+        }
+    };
+
+    this.clear = function(scene, view_mode) {
+
     }
 };
 
@@ -70,15 +89,90 @@ RNAMAKE.Residue = function(rtype, name, num, chain_id, i_code) {
     this.i_code   = i_code;
     if(this.i_code == undefined) {  this.i_code = ""; }
     this.atoms = [];
+    this.bonds = [];
 
     this.setup_atoms = function(atoms) {
         for(var i in atoms) {
+            this.atoms.push(null);
+        }
+
+        for(i in atoms) {
             //var name_change
             if(this.rtype.atom_map.hasOwnProperty(atoms[i].name)) {
                 var pos = this.rtype.atom_map[atoms[i].name];
+                this.atoms[pos] = atoms[i];
+                if(this.atoms[pos].name[0] == "N") {
+                    this.atoms[pos].color = 'blue';
+                }
+                if(this.atoms[pos].name[0] == "C") {
+                    this.atoms[pos].color = 'gray';
+                }
+                if(this.atoms[pos].name[0] == "P") {
+                    this.atoms[pos].color = 'orange';
+                }
+                if(this.atoms[pos].name[0] == "O") {
+                    this.atoms[pos].color = 'red';
+                }
             }
+
         }
     };
+
+    this.draw = function(scene, draw_mode) {
+        this.draw_mode = draw_mode;
+        if(this.draw_mode == RNAMAKE.DrawMode.STICKS) {
+            this.atoms.forEach(function(a) {
+                if(a == null) { return true; }
+                a.draw(scene, draw_mode);
+            });
+
+            this.draw_bonds(scene);
+        }
+    }
+
+    this.draw_bonds = function(scene) {
+        this.draw_bond(0, 1, scene);
+        this.draw_bond(0, 2, scene);
+        this.draw_bond(0, 3, scene);
+        this.draw_bond(3, 4, scene);
+        this.draw_bond(4, 5, scene);
+        this.draw_bond(5, 6, scene);
+        this.draw_bond(5, 7, scene);
+        this.draw_bond(5, 7, scene);
+        this.draw_bond(7, 8, scene);
+        this.draw_bond(6, 9, scene);
+        this.draw_bond(7, 10, scene);
+        this.draw_bond(9, 10, scene);
+        this.draw_bond(10, 11, scene);
+    };
+
+    this.draw_bond = function(i, j, scene) {
+        if(this.atoms[i] == null || this.atoms[j] == null) { return; }
+        var a1 = this.atoms[i];
+        var a2 = this.atoms[j];
+        var direction = new THREE.Vector3().subVectors(a2.obj.position, a1.obj.position);
+        var arrow = new THREE.ArrowHelper(direction.clone().normalize(), a1.obj.position);
+        var rotation = new THREE.Euler().setFromQuaternion(arrow.quaternion);
+        var edgeGeometry = new THREE.CylinderGeometry( 0.15, 0.15, direction.length(), 10, 4 );
+        //var edgeGeometry = new THREE.CylinderGeometry( 4, 4, 10, 10, 4 );
+
+        var material =  new THREE.MeshBasicMaterial( { color: 'gray' } );
+        var edge = new THREE.Mesh(edgeGeometry, material);
+        edge.rotation.copy(rotation);
+        //var vect = new THREE.Vector3().addVectors(a1.obj.position, direction.multiplyScalar(0.5));
+        var vect = a1.obj.position.clone();
+        var half_direct = direction.multiplyScalar(0.5);
+        vect.x = parseFloat(vect.x) + parseFloat(half_direct.x);
+        vect.y = parseFloat(vect.y) + parseFloat(half_direct.y);
+        vect.z = parseFloat(vect.z) + parseFloat(half_direct.z);
+        edge.position.copy(vect);
+        //edge.position.copy(a1.obj.position);
+        edge.castShadow = true;
+        edge.receiveShadow = true;
+        self.bonds.push(edge);
+        scene.add(edge);
+
+    }
 };
 
 RNAMAKE.str_to_atom = function(s) {
